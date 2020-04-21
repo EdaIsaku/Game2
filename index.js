0993b798adb85da655e6e42bc7c67f3cba3d71dd
edaisaku <60227449+edaisaku@users.noreply.github.com>
2020-04-21 20:56:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // detect button click
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+var nrButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < nrButtons ; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
